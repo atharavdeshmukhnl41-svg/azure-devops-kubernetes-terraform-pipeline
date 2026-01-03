94f91f10560be69285dd63ef6a45e3d4c426dee6
Atharv <atharvdeshmukh41@gmail.com>
2026-01-03 07:19:34 +0530

    Fix main.tf1

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
-
-  backend "azurerm" {}
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "resource_group" {
   name     = "${var.resource_group}_${var.environment}"
   location = var.location
@@ -35,7 +20,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   default_node_pool {
     name            = "agentpool"
     node_count      = var.node_count
-    vm_size         = "Standard_D2ps_v6"
+    vm_size         = "Standard_DS1_v2"
   }
 
   service_principal {
@@ -47,12 +32,12 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
     Environment = var.environment
   }
 }
-# terraform {
-#   backend "azurerm" {
 
-#     # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
-#     # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
-#     # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
-#     # container_name="<<storage_account_container_name>>" #OVERRID
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    # storage_account_name="<<storage_account_name>>" #OVERRIDE in TERRAFORM init
+    # access_key="<<storage_account_key>>" #OVERRIDE in TERRAFORM init
+    # key="<<env_name.k8s.tfstate>>" #OVERRIDE in TERRAFORM init
+    # container_name="<<storage_account_container_name>>" #OVERRIDE in TERRAFORM init
+  }
+}
